a0640ddf670f5cc07af861cb66474a5b51708ba3
robi <kali_robi@hotmail.com>
2024-05-28 09:51:23 +0300

    first migartions

--- shipmentregister/migrate_db.py
+++ shipmentregister/migrate_db.py
@@ -1,17 +1,17 @@
 from django.db import migrations
 
 def migrate_mode_of_transport(apps, schema_editor):
-    YourModel = apps.get_model('yourappname', 'YourModel')
-    ModeOfTransport = apps.get_model('yourappname', 'ModeOfTransport')
-    for obj in YourModel.objects.all():
-        mode_of_transport, created = ModeOfTransport.objects.get_or_create(name=obj.mode_of_transport)
+    shipmentDetailsModel = apps.get_model('shipmentregister', 'ShipmentDetailsModel')
+    modeOfTransportmodel = apps.get_model('shipmentregister', 'ModeOfTransport')
+    for obj in shipmentDetailsModel.objects.all():
+        mode_of_transport, created = modeOfTransportmodel.objects.get_or_create(name=obj.mode_of_transport)
         obj.new_mode_of_transport = mode_of_transport
         obj.save()
 
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('yourappname', 'previous_migration'),
+        ('yourappname', '0003_alter_shipmentdetailsmodel_mode_of_transport'),
     ]
 
     operations = [
